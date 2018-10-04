76a332f582b39f0a22045d9cab0e376fe7e8b2eb
Aakash Singh <singh.aakash1801@gmail.com>
2018-10-04 15:23:54 +0530

    Register service worker
    Register service worker  in main.js file

---
+++ main.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+            console.log('Service worker successfully registered on scope', registration.scope);
+        }).catch(function (error) {
+            console.log('Service worker failed to register', error);
+        });
+    });
+}
