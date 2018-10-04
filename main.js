cb9eccd0572fdd2092a99caa19a55e6ce7c19e80
Ajit Singh <ajit.singh2905@gmail.com>
2018-10-04 16:25:59 +0530

    format pwa related js files

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            console.log('Service worker successfully registered on scope', registration.scope);
-        }).catch(function (error) {
-            console.log('Service worker failed to register', error);
-        });
-    });
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
+      console.log('Service worker successfully registered on scope', registration.scope)
+    }).catch(function (error) {
+      console.log('Service worker failed to register', error)
+    })
+  })
 }
--- sw.js
+++ sw.js
@@ -1,80 +1,76 @@
 
-var cacheName = 'WWW-EXAMPLE-COM-V1';
+var cacheName = 'WWW-EXAMPLE-COM-V1'
 
 var filesToCache = [
-    '/index.html',                // index.html
-    '/main.js',
-    '/css/style.css',
-    '/images/github.svg',
-    '/fonts/impact.woff'
+  '/index.html', // index.html
+  '/main.js',
+  '/css/style.css',
+  '/images/github.svg',
+  '/fonts/impact.woff'
 
-];
+]
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(function(cache) {
-                console.info('[sw.js] cached all files');
-                return cache.addAll(filesToCache);
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function (cache) {
+        console.info('[sw.js] cached all files')
+        return cache.addAll(filesToCache)
+      })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response
+        } else {
+          // clone request stream
+          // as stream once consumed, can not be used again
+          var reqCopy = event.request.clone()
+
+          return fetch(reqCopy, { credentials: 'include' }) // reqCopy stream consumed
+            .then(function (response) {
+              // bad response
+              // response.type !== 'basic' means third party origin request
+              if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response // response stream consumed
+              }
+
+              // clone response stream
+              // as stream once consumed, can not be used again
+              var resCopy = response.clone()
+
+              // ================== IN BACKGROUND ===================== //
+
+              // add response to cache and return response
+              caches.open(cacheName)
+                .then(function (cache) {
+                  return cache.put(reqCopy, resCopy) // reqCopy, resCopy streams consumed
+                })
+
+              // ====================================================== //
+
+              return response // response stream consumed
             })
-    );
-});
+        }
+      })
+  )
+})
 
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if(response){
-                    return response
-                }
-                else{
-                    // clone request stream
-                    // as stream once consumed, can not be used again
-                    var reqCopy = event.request.clone();
-
-                    return fetch(reqCopy, {credentials: 'include'}) // reqCopy stream consumed
-                        .then(function(response) {
-                            // bad response
-                            // response.type !== 'basic' means third party origin request
-                            if(!response || response.status !== 200 || response.type !== 'basic') {
-                                return response; // response stream consumed
-                            }
-
-                            // clone response stream
-                            // as stream once consumed, can not be used again
-                            var resCopy = response.clone();
-
-
-                            // ================== IN BACKGROUND ===================== //
-
-                            // add response to cache and return response
-                            caches.open(cacheName)
-                                .then(function(cache) {
-                                    return cache.put(reqCopy, resCopy); // reqCopy, resCopy streams consumed
-                                });
-
-                            // ====================================================== //
-
-
-                            return response; // response stream consumed
-                        })
-                }
-            })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys()
-            .then(function(cacheNames) {
-                return Promise.all(
-                    cacheNames.map(function(cName) {
-                        if(cName !== cacheName){
-                            return caches.delete(cName);
-                        }
-                    })
-                );
-            })
-    );
-});
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.map(function (cName) {
+            if (cName !== cacheName) {
+              return caches.delete(cName)
+            }
+          })
+        )
+      })
+  )
+})
